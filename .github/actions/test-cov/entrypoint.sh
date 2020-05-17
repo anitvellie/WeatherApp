27d45fffa3b704fa0b7cfc0c5039c6ddb53730b7
Alevtina Anishchenko <allieanischenko@gmail.com>
2020-05-17 23:01:26 +0300

    actions: update test cov to report valid data

--- .github/actions/test-cov/entrypoint.sh
+++ .github/actions/test-cov/entrypoint.sh
@@ -1,16 +1,6 @@
 #!/bin/sh -l
 
-BIN_PATH="$(swift build --show-bin-path)"
-XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
-
-COV_BIN=$XCTEST_PATH
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    f="$(basename $XCTEST_PATH .xctest)"
-    COV_BIN="${COV_BIN}/Contents/MacOS/$f"
-fi
-
-llvm-cov report \
-    "${COV_BIN}" \
-    -instr-profile=.build/debug/codecov/default.profdata \
-    -ignore-filename-regex=".build|Tests" \
-    -use-color
+xcrun llvm-cov report \
+     .build/x86_64-apple-macosx/debug/WeatherAppPackageTests.xctest/Contents/MacOS/WeatherAppPackageTests \
+     -instr-profile=.build/debug/codecov/default.profdata \
+     -use-color
