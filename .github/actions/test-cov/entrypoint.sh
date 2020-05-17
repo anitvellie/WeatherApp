ade1059d185805704b41e3a8a700ab75a82430e0
Alevtina Anishchenko <allieanischenko@gmail.com>
2020-05-17 23:32:52 +0300

    actions: fix directory

--- .github/actions/test-cov/entrypoint.sh
+++ .github/actions/test-cov/entrypoint.sh
@@ -2,7 +2,9 @@
 
 swift test --enable-code-coverage
 
+# find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
+
 llvm-cov report \
-     .build/x86_64-apple-macosx/debug/WeatherAppPackageTests.xctest/Contents/MacOS/WeatherAppPackageTests \
-     -instr-profile=.build/debug/codecov/default.profdata \
+     /github/workspace/.build/x86_64-unknown-linux-gnu/debug/WeatherAppPackageTests.xctest \
+     -instr-profile=/github/workspace/.build/debug/codecov/default.profdata \
      -use-color
