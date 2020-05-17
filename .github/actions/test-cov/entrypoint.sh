449baf5df91baa0e776558b57dcbfa0e9d05821e
Alevtina Anishchenko <allieanischenko@gmail.com>
2020-05-17 22:19:37 +0300

    actions: try another test cov

--- .github/actions/test-cov/entrypoint.sh
+++ .github/actions/test-cov/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -l
 
-swift test --enable-code-coverage
+BIN_PATH="$(swift build --show-bin-path)"
+XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
 
- llvm-cov report \
-     .build/x86_64-apple-macosx/debug/WeatherAppPackageTests.xctest/Contents/MacOS/WeatherAppPackageTests \
-     -instr-profile=.build/debug/codecov/default.profdata \
-     -ignore-filename-regex=".build|Tests" \
-     -use-color 
+COV_BIN=$XCTEST_PATH
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    f="$(basename $XCTEST_PATH .xctest)"
+    COV_BIN="${COV_BIN}/Contents/MacOS/$f"
+fi
+
+llvm-cov report \
+    "${COV_BIN}" \
+    -instr-profile=.build/debug/codecov/default.profdata \
+    -ignore-filename-regex=".build|Tests" \
+    -use-color
